8d86ef36a64d7c25b1bd5831caf060b4e3f132f3
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-06 23:58:11 -0500

    Actualizar info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -5,14 +5,14 @@ var handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => '')
 let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografia = await conn.fetchStatus('51935531943' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('51904049358' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('5217294888993'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
+[devnum, `${await conn.getName('51935531943'+'@s.whatsapp.net')}`, `🦉 Creador`, dev, email, `🇵🇪 Peru`, `https://www.youtube.com/@Edwardofc`, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 Atro-Lite-Bot-MD`, `📵 No Hacer Spam`, email, `🇵🇪no hacer spam`, `https://github.com/Edwardofc/Atro-Lite-Bot-MD`, biobot]
 ], m)
   
 }
