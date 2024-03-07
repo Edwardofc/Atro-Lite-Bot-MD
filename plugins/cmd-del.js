97ad9e946c1fc601101f346777591f016bb65ded
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-06 23:37:02 -0500

    Actualizar cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -5,7 +5,7 @@ if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
 if (!hash) return conn.reply(m.chat, `🎌 *Ingrese el código de la asignación*\n\nUtilice !listcmd`, m, fake, )
 
 let sticker = global.db.data.sticker
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '🚩 *¡Solo mi propietario puede usar esta función!*', m, fake, )
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '⛔ *¡Solo mi propietario puede usar esta función!*', m, fake, )
 delete sticker[hash]
 conn.reply(m.chat, `✅ *Eliminado correctamente*`, m, fake, )
 
