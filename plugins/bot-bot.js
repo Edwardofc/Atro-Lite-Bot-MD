e2a38eacee6392f39827703a5c86120039c23897
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-06 23:12:19 -0500

    Actualizar bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '👋 *Hola estoy aquí para ayudarte dime en que puedo ayudarte, si deceas ver más de mi coloca el siguiente comando .menu*', m, fake, )}
+conn.reply(m.chat, '👋 *Hola estoy aquí dime en que puedo ayudarte, si deceas ver más de mi coloca el siguiente comando .menu*', m, fake, )}
 
 return !0
 
