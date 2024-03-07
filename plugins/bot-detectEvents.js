6732df94391ca6a0c414e88ca5a24d6c2f2deaeb
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-06 23:14:53 -0500

    Actualizar bot-detectEvents.js

--- plugins/bot-detectEvents.js
+++ plugins/bot-detectEvents.js
@@ -27,7 +27,7 @@ for (const file of files) {
 if (file.includes(uniqid)) {
 await fs.unlink(path.join(sessionPath, file));
 filesDeleted++;
-console.log(`⚠️ Eliminacion session (PreKey) que provocan el undefined el chat`)}}}
+console.log(`⚠️🔱 Eliminacion session (PreKey) que provocan el undefined el chat`)}}}
 
 if (chat.detect2 && m.messageStubType == 21) {
 await this.sendMessage(m.chat, { text: `🚩 ${usuario} *Ha cambiado el nombre del grupo*`, mentions: [m.sender], mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id) }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}) 
@@ -39,20 +39,20 @@ await this.sendMessage(m.chat, { text: `🚩 ${usuario} *Ha cambiado la imágen 
 await this.sendMessage(m.chat, { text: `🚩 ${usuario} *Ha modificado la descripción!*\n\nNueva descripción:\n\n${m.messageStubParameters[0]}`, mentions: [m.sender] }, { quoted: fliveLoc, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 
 } else if (chat.detect2 && m.messageStubType == 25) {
-await this.sendMessage(m.chat, { text: `🚩 *Ahora ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden editar la información del grupo*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+await this.sendMessage(m.chat, { text: `👤 *Ahora ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden editar la información del grupo*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 
 } else if (chat.detect2 && m.messageStubType == 26) {
 await this.sendMessage(m.chat, { text: `🚩 *El grupo ha sido ${m.messageStubParameters[0] == 'on' ? 'cerrado' : 'abierto'}*\n\n${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden enviar mensajes`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 
 } else if (chat.detect2 && m.messageStubType == 29) {
-let txt1 = `🚩 *Nuevo admin*\n\n`
+let txt1 = `🔅 *Nuevo admin*\n\n`
 txt1 += `Nombre: @${m.messageStubParameters[0].split`@`[0]}\n`
 txt1 += `Le otorgó admin: @${m.sender.split`@`[0]}`
 
 await conn.sendMessage(m.chat, {text: txt1, mentions: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": links, "sourceUrl": links}}})
 
 } else if (chat.detect2 && m.messageStubType == 30) {
-let txt2 = `🚩 *Un admin menos*\n\n`
+let txt2 = `👇 *Un admin menos*\n\n`
 txt2 += `Nombre: @${m.messageStubParameters[0].split`@`[0]}\n`
 txt2 += `Le quitó admin: @${m.sender.split`@`[0]}`
 
