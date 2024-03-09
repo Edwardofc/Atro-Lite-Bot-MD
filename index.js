84268d8678c37bba904d7c8c6e696d5f86e29a0c
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-09 00:18:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Curiosity\nBot\nMD', {
+say('AtroLite\nBot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['magenta', 'cyan']
